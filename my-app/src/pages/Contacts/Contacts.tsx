f9b5a513b912603748fe0bee5ed3c151edd0bede
GitGity <afssg@yahoo.com>
2022-11-21 19:19:42 +0200

    minor fixes and renaming

--- my-app/src/pages/Contacts/Contacts.tsx
+++ my-app/src/pages/Contacts/Contacts.tsx
@@ -30,9 +30,9 @@ export type contactType = {
 		bs: string;
 	};
 };
-type allContactType = Array<contactType>;
 
-const AddNewContactForm = ({ visible, onCreate, onCancel, contacts }: any) => {
+
+const AddNewContactForm = ({ visible, showAddContactModal, onCancel, contacts }: any) => {
 	const [form] = Form.useForm();
 	const [idCounter, setIdCounter] = useState(contacts.length + 1);
 
@@ -50,7 +50,7 @@ const AddNewContactForm = ({ visible, onCreate, onCancel, contacts }: any) => {
 					.validateFields()
 					.then((values) => {
 						form.resetFields();
-						onCreate(values);
+						showAddContactModal(values);
 					})
 					.catch((info) => {
 						console.log("Validate Failed:", info);
@@ -110,7 +110,7 @@ const AddNewContactForm = ({ visible, onCreate, onCancel, contacts }: any) => {
 
 const Contacts = () => {
 	const [isLoading, setLoading] = useState(true);
-	const [contacts, setContacts] = useState<allContactType>([]);
+	const [contacts, setContacts] = useState<contactType[]>([]);
 	const [searchTerm, setSearchTerm] = useState<string>("");
 	const link = "https://jsonplaceholder.typicode.com/users";
 	const [isModalVisible, setIsModalVisible] = useState(false);
@@ -165,13 +165,13 @@ const Contacts = () => {
 		}
 	}, []);
 
-	const onCreate = (values: contactType) => {
+	const showAddContactModal = (values: contactType) => {
 		console.log("Received values of form: ", values);
 		setIsModalVisible(false);
-		AddContact(values);
+		addContact(values);
 	};
 
-	const AddContact = (newContact: contactType) => {
+	const addContact = (newContact: contactType) => {
 		const usersAfterAdd = [...contacts, newContact];
 		setContacts(usersAfterAdd);
 		localStorage.setItem("contacts", JSON.stringify(usersAfterAdd));
@@ -199,7 +199,7 @@ const Contacts = () => {
 
 					<AddNewContactForm
 						visible={isModalVisible}
-						onCreate={onCreate}
+						showAddContactModal={showAddContactModal}
 						onCancel={() => {
 							setIsModalVisible(false);
 						}}
