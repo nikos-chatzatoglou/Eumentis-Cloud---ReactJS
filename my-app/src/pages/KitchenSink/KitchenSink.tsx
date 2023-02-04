66bf478f7d8cffcedf9f62a10b0904159eef9cb5
nikos-chatzatoglou <donucore@gmail.com>
2023-02-04 13:15:41 +0200

    remove Bootstrap

--- my-app/src/pages/KitchenSink/KitchenSink.tsx
+++ my-app/src/pages/KitchenSink/KitchenSink.tsx
@@ -1,6 +1,4 @@
 import { Button, Card } from "antd";
-import { ButtonGroup, CardGroup } from "react-bootstrap";
-import SearchBar from "../../components/SearchBar/SearchBar";
 
 const KitchenSink = () => {
 	return (
