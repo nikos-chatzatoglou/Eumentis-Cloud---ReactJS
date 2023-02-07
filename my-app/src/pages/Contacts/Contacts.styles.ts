2cd22125a6f27c4353dabb9ddd28eb42b87348fe
nikos-chatzatoglou <donucore@gmail.com>
2023-02-07 18:30:20 +0200

    bg on contacts page full height now

--- my-app/src/pages/Contacts/Contacts.styles.ts
+++ my-app/src/pages/Contacts/Contacts.styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 background: linear-gradient(180deg, #fff 0%, #B5B2AF 250%);
+height: 100vh;
 `
 export const StyledButton = styled(Button)`
     margin: 25px 0 0 70px;
